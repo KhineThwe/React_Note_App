1f6d6f47b7b71fb4d1baa298c96c0265b2aa2f68
Khine <khinezar70122107@gmail.com>
2023-01-08 19:03:38 -1200

    adding pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './components/Header'
-
+import Footer from './components/Footer'
+import NotesListPage from './pages/NotesListPage';
 function App() {
   return (
     <div className='App'>
       <Header/>
+      <NotesListPage/>
+      <Footer/>
     </div>
   );
 }
