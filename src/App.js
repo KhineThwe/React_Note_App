3b35fd1bbd2629d12fd831ad8e7f69dbfd9e4df5
Khine <khinezar70122107@gmail.com>
2023-01-08 20:07:15 -1200

    react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import NotesListPage from './pages/NotesListPage';
+import NotePage from './pages/NotePage'
+
 function App() {
   return (
-    <div className='App'>
-      <Header/>
-      <NotesListPage/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className='App'>
+        <Header/>
+        <Routes>
+        <Route path='/' element={<NotesListPage/>}/>
+        <Route path='/note/:id' element={<NotePage/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
